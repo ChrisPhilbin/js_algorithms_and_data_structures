cefd1faa65f201d557e7d822e569f902d85dc845
Chris Philbin <chris@chrisphilbin.net>
2021-06-18 08:44:20 -0400

    Multiple pointers using for loop

--- multiple_pointers.js
+++ multiple_pointers.js
@@ -1,3 +1,6 @@
+//return the first pair of numbers whose absolute value when added
+//equals zero
+
 const sumZero = (arr) => {
   let left = 0;
   let right = arr.length - 1;
@@ -15,3 +18,26 @@ const sumZero = (arr) => {
 };
 
 console.log(sumZero([-4, -3, -2, -1, 0, 1, 2, 3, 10]));
+
+//given an array of sorted numbers, return the number of unique numbers using the
+//two pointer method
+
+//ie: uniqueNumbers([1, 1, 1, 1, 2]) => should return 2
+//ie: uniqueNumbers([2, 2, 2, 2]) => should return 1
+//ie: uniqueNumbers([1, 2, 3, 4, 5]) => should return 5
+
+const uniqueNumbers = (arr) => {
+  if (arr.length === 0) return 0;
+  let i = 0;
+
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+
+  return i + 1;
+};
+
+console.log(uniqueNumbers([1, 1, 1, 2, 3, 4]), "Number of unique numbers");
