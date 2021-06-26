f1d319d374c4bf4b0e6dd54006d224cabdfc9c36
Chris Philbin <chris@chrisphilbin.net>
2021-06-26 17:26:36 -0400

    Finishing merge sort

--- sorting/merge_sort.js
+++ sorting/merge_sort.js
@@ -1,7 +1,14 @@
 //n log(n) runtime
 
-let arr1 = [1, 3, 5, 7, 9];
-let arr2 = [2, 4, 6, 8, 10];
+let arr = [1, 7, 3, 5, 6, 1, 2, 9, 12, 19, 22, 39];
+
+const mergeSort = (arr) => {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+};
 
 const merge = (arr1, arr2) => {
   let results = [];
@@ -17,18 +24,18 @@ const merge = (arr1, arr2) => {
       results.push(arr2[j]);
       j++;
     }
+  }
 
-    while (i < arr1.length) {
-      results.push(arr1[i]);
-      i++;
-    }
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
 
-    while (j < arr2.length) {
-      results.push(arr2[j]);
-      j++;
-    }
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
   }
   return results;
 };
 
-console.log(merge(arr1, arr2));
+console.log(mergeSort(arr));
